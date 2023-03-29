3170c24f7b08eb9f4d3f5a07db298215d5dd6c72
ImFerd <ferdaboubakr@gmail.com>
2023-03-29 11:40:20 -0700

    sum and print tasks

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+i#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+* sum_them_all - returns the sum of all it's parameters
+* @n: first arguement
+* Return: integer
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	/*define variadic list*/
+	va_list numbers;
+
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+	/*initiate list*/
+	va_start(numbers, n);
+
+	/*loop through members of the list and sum each*/
+	for (i = 0; i < n; i++)
+		sum += va_arg(numbers, int);
+
+	va_end(numbers);
+
+	return (sum);
+}
