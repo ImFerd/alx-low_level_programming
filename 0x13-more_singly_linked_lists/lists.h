e3967efa28dd9dc7f8bb356ca2e5d68f8a6aed11
ImFerd <ferdaboubakr@gmail.com>
2023-04-04 10:43:54 -0700

    added 8th task + modded lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -24,5 +24,6 @@ void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 listint_t *add_nodeint_end(listint_t **head, const int n);
+int sum_listint(listint_t *head);
 
 #endif /*LISTS_H*/
